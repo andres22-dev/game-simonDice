bbe48ff7c361aa1f213cf339e90f34aae0fa22ed
andres22 <afpinchao8@misena.edu.co>
2021-01-24 13:27:17 -0500

    agregando clase juego y obteniendo elementos de html

---
+++ index.js
@@ -0,0 +1,38 @@
+// Creamos la clase juego, esta clase tendra toda la logica del video juego
+
+
+// Obtenemos el para ocultar el boton
+const buttonStart = document.querySelector('#btnEmpezar');
+
+//Obtenemos el resto de los botones 
+
+const celeste = document.querySelector('#celeste');
+const violeta = document.querySelector('#celeste');
+const naranja = document.querySelector('#celeste');
+const verde = document.querySelector('#celeste');
+
+class Juego{
+
+    constructor(){
+
+        this.inicializar()
+
+    }
+
+    inicializar(){
+
+    
+
+    }
+
+
+
+}
+
+function empezarJuego(){
+
+    // iniciamos el juego creando un nuevo objeto de la clase juego
+
+    var juego = new Juego()
+
+}
