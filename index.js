30a1f6366efc6dcf410bd7a4a1b9a17407372367
andres22 <afpinchao8@misena.edu.co>
2021-01-24 23:27:04 -0500

    agregando color elegido

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const buttonStart = document.querySelector('#btnEmpezar');
 
 
@@ -7,13 +6,15 @@ const violeta = document.querySelector('#violeta');
 const naranja = document.querySelector('#naranja');
 const verde = document.querySelector('#verde');
 
+//agregamos el ultimo nivel
+const ULTIMO_NIVEL = 10
 class Juego{
 
     constructor(){
 
         this.inicializar()
         this.generarSecuencia()
-        this.siguienteNivel()
+        setTimeout(this.siguienteNivel(), 500)
         
     }
 
@@ -22,7 +23,7 @@ class Juego{
         
         buttonStart.classList.add('hide');
 
-        this.nivel = 7;
+        this.nivel = 1;
 
         this.colores = {
      
@@ -37,16 +38,20 @@ class Juego{
 
     generarSecuencia(){
 
-        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+        this.secuencia = new Array(ULTIMO_NIVEL).fill(0).map(n => Math.floor(Math.random() * 4))
         
     }
 
   
     siguienteNivel(){
 
+        //agregamos subnivel que empieza en 0 en cada uno de los niveles
+        this.subnivel = 0
+
+        this.nombreAtributo = 'valor'
 
         this.ilumonarSecuencia()
-        //Llamamos a la funcion agregarEventosClick
+        
         this.agregarEventosClick()
         
 
@@ -64,6 +69,25 @@ class Juego{
                 return 'naranja'
             case 3:
                 return 'verde'
+
+        }
+
+    }
+
+    //agregamos la funcion transformar color a numero y recibe un color por parametro
+
+    transformarColorANumero(color){
+
+        switch(color){
+
+            case 'celeste':
+                return 0
+            case 'violeta':
+                return 1
+            case 'naranja':
+                return 2
+            case 'verde':
+                return 3
 
         }
 
@@ -101,31 +125,72 @@ class Juego{
 
         }
 
-        //agregamos la funcion eventos click
+     
         agregarEventosClick(){
-        //por cada uno de los colores le agregaremos el eventos click
-        //este evento se ejecutara asincronamente 
-
-        /* Lo que hacemos com bind()  es decirle ¡N0! tu no seras un elemento de HTML
-            tu seras Game(), digamos que cambia su forma de pensar del this.
-        
-        */
+       
             this.colores.celeste.addEventListener('click', this.elegirColor.bind(this))
             this.colores.verde.addEventListener('click', this.elegirColor.bind(this))
             this.colores.violeta.addEventListener('click', this.elegirColor.bind(this))
             this.colores.naranja.addEventListener('click', this.elegirColor.bind(this))
         }
 
-        // declaramos la funcion elegir color
+        //agregamos funcion eliminar evento en ves de add ponemos remove para que se borre
+        eliminarEventosClick(){
+       
+            this.colores.celeste.removeEventListener('click', this.elegirColor.bind(this))
+            this.colores.verde.removeEventListener('click', this.elegirColor.bind(this))
+            this.colores.violeta.removeEventListener('click', this.elegirColor.bind(this))
+            this.colores.naranja.removeEventListener('click', this.elegirColor.bind(this))
+        }
+
+
+
         elegirColor(event){
-        //obtenemos el evento por el parametro y podremos imprimirlo 
-                //console.log(event)
-        /* Imprimimos this y nos damos cuenta que aqui this hace referencia
-           a los botones porque this es un escuchar de eventos que representa
-           el html el cual le asignamos ese evento
-           
-           Podremos cambiar a quien  hace referencia this con bind
-           */
+       
+        //obtenemos el nombre del color que esta definido en la propiedad dataset
+        //que previamente definimos en nuestro elemento de html
+          const nombreColor = event.target.dataset.color
+          // asignamos la funcion transformar color a numero a numeroColor y
+          // a esa funcion le pasamos el nombre color por parametro
+          const numeroColor = this.transformarColorANumero(nombreColor)
+          this.iluminarColor(nombreColor)
+
+          // si el numero del color es igual al que esta en la secuencia
+
+          if(numeroColor === this.secuencia[this.subnivel]){
+
+            //el subnivel sube
+            this.subnivel++
+
+            // si el subnivel es igual el nivel 
+            if(this.subnivel === this.nivel){
+
+                // el nivel aumenta uno
+                this.nivel++
+                // si el usuario pasa de nivel ya no tiene porque elegir colores
+
+                //this.eliminarEventosClick()
+
+                if(this.nivel === ULTIMO_NIVEL + 1){
+
+                    console.log(`ganaste`)
+
+                }else{
+                    //si no es el ultimo nivel tiene que avanzar de nivel
+                    //agregamos delay al pasar de nivel
+
+                    setTimeout(this.siguienteNivel.bind(this), 1500)
+
+
+                }
+
+            }
+
+
+          }else{
+
+            console.log(`Perdio`);
+          }
 
 
         }
@@ -133,9 +198,6 @@ class Juego{
     }
 
         
-   
-
-
 
 function empezarJuego(){
 
